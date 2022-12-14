096fea208dadefad7fd087d55a9106f8b66ae37e
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-12-14 11:58:35 +0500

    DirectionalDetailScreen.kt added string file changed

---
+++ app/src/main/java/uz/gita/saiga_user/presentation/directional_taxi/details/DirectionalDetailScreen.kt
@@ -0,0 +1,8 @@
+package uz.gita.saiga_user.presentation.directional_taxi.details
+
+import androidx.fragment.app.Fragment
+import uz.gita.saiga_user.R
+
+// Created by Jamshid Isoqov on 12/14/2022
+class DirectionalDetailScreen : Fragment(R.layout.screen_directional_detail){
+}
